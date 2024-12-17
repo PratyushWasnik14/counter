196ff2a0f57f64f52ce4c7e59b8f402bf6fe2291
PratyushWasnik14 <pratyushwasnik01@gmail.com>
2024-12-17 17:54:00 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// store count and ini as zero
+// listen for clicks
+// increment when button is clicked
+// change count-el in the html to reflect the increment
+
+/*
+let countEl = document.getElementById("count-el"); // Get the counter element
+let saveEl = document.getElementById("save-el" );
+let count = 0; // Initialize counter variable
+
+function increment() {
+    count+=1; // Increment the count
+    countEl.textContent = count; // Update the displayed count
+    }
+
+function save(){
+    saveEl.textContent += count+" - ";
+    countEl.textContent = 0;
+    count=0
+}
+*/
+
+
+
